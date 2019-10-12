3be3dd614e7672b3d1289382c35bda4a29e18143
Huan Nguyen <huancal1984@gmail.com>
2019-10-11 17:28:56 -0700

    50% logic done

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    var generate_random = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var targetScore = generate_random(19, 120);
+    var crystal_1 = generate_random(1, 12);
+    var crystal_2 = generate_random(1, 12);
+    var crystal_3 = generate_random(1, 12);
+    var crystal_4 = generate_random(1, 12);
+
+
+    $('#Comp-Random-Num').text(targetScore);
+
+    var wins = 0;
+    var losses = 0;
+    var playerscore = 0;
+
+    $('#button1').on('click', function () {
+        playerscore += crystal_1;
+        $('#finalScore').append = playerscore
+        console.log('button1click', playerscore);
+        winscheck()
+    });
+
+    $('#button2').on('click', function () {
+        playerscore += crystal_2;
+        $('#finalScore').html = playerscore
+        console.log('button1click', playerscore);
+        winscheck()
+    });
+
+    $('#button3').on('click', function () {
+        playerscore += crystal_3;
+        $('#finalScore').html = playerscore
+        console.log('button1click', playerscore);
+        winscheck()
+    });
+
+    $('#button4').on('click', function () {
+        playerscore += crystal_4;
+        $('#finalScore').html = playerscore
+        console.log('button1click', playerscore);
+        winscheck()
+    });
+
+    var winscheck = function () {
+        if (targetScore === playerscore) {
+            wins++
+            $('#wins').text(("Wins: " + wins));
+            alert('Congrats You Won');
+            resetgame();
+        } else if (playerscore > targetScore) {
+            losses++
+            $('#Losses').text(("Losses: " + losses));
+            alert('Sorry try again');
+            resetgame();
+        }
+    };
+
+    var resetgame = function () {
+        targetScore = generate_random(19, 120);
+        crystal_1 = generate_random(1, 12);
+        crystal_2 = generate_random(1, 12);
+        crystal_3 = generate_random(1, 12);
+        crystal_4 = generate_random(1, 12);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
