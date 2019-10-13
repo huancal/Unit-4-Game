5af18db7e4ab1309e1ec2fde815fe0c3ba6f87fa
Huan Nguyen <huancal1984@gmail.com>
2019-10-12 20:21:41 -0700

    crystal did not reset, changed js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -59,27 +59,11 @@ $(document).ready(function () {
         targetScore = generate_random(19, 120);
         $('#Comp-Random-Num').text(targetScore);
         playerscore = 0;
-        $('#finalScore').html(playerscore) = 0;
         crystal_1 = generate_random(1, 12);
         crystal_2 = generate_random(1, 12);
         crystal_3 = generate_random(1, 12);
         crystal_4 = generate_random(1, 12);
+        $('#finalScore').html(playerscore) = 0;
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
